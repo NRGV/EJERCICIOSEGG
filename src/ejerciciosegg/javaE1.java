30cbf0dcb60e2f36d3906cf2ab83cfb5c2d6419a
Nelson Rodri Gonzalez <gonzalezvallejosnelson.laboral@gmail.com>
2023-03-13 14:34:28 -0300

    ejercicio 5 guia 3 menu con subprograma

--- src/ejerciciosegg/javaE1.java
+++ src/ejerciciosegg/javaE1.java
@@ -5,6 +5,7 @@
  */
 package ejerciciosegg;
 
+
 import java.util.Scanner;
 
 /**
@@ -15,34 +16,61 @@ public class javaE1 {
 
     /**
      * @param args the command line arguments
+     * 
+     * Realizar un programa que pida dos números enteros positivos por teclado y muestre por pantalla el siguiente menú:
+     * El usuario deberá elegir una opción y el programa deberá mostrar el resultado por pantalla y luego volver al menú. 
+     * El programa deberá ejecutarse hasta que se elija la opción 5. 
+     * Tener en cuenta que, si el usuario selecciona la opción 5, en vez de salir del programa directamente, 
+     * se debe mostrar el siguiente mensaje de confirmación: ¿Está seguro que desea salir del programa (S/N)? 
+     * Si el usuario selecciona el carácter ‘S’ se sale del programa, caso contrario se vuelve a mostrar el menú.
      */
     public static void main(String[] args) {
         
-        int numero1 , numero2, result;
-        
-        
-        Scanner read = new Scanner(System.in);
-        
-        
+        int numero1, numero2;
+        numero1 = 0;
+        numero2 = 0;
+      
+        Scanner read;
+        read = new Scanner(System.in);
+         System.out.println("/////////////////////////////////////////////////");
+        System.out.println("//      Bienvenidos a la calculadora            //");
+        System.out.println("////////////* 1): Sumar       ////////////////////");
+        System.out.println("////////////* 2): Restar      ////////////////////");
+        System.out.println("////////////* 3): Multiplicar ////////////////////");
+        System.out.println("////////////* 4): Dividir     ////////////////////");
+        System.out.println("////////////* 5): Salir       ////////////////////");
+        System.out.println("//////////////////////////////////////////////////");
         System.out.println("Ingrese dos numero enteros");
         numero1 = read.nextInt();
         numero2 = read.nextInt();
-        result = numero1 + numero2;
-        System.out.println("El resultado es " + result );
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
+       funcionmenu(numero1,numero2);
     }
-    
-}
+
+    private static void funcionmenu(int numero1, int numero2) {
+        
+        int opmenu
+        String opcionsalir;
+        opmenu = 0; 
+        Scanner readopcion = new Scanner(System.in);
+        System.out.println("Ingrese una opcion para realizar una operacion");
+        opmenu5 = readopcion.nextInt();
+       
+        switch (opmenu ) {
+            case 1:
+                 System.out.println(numero1 + numero2);
+                break;
+            case 2:
+                 System.out.println(numero1 - numero2);
+                break;
+            case 3:
+                 System.out.println(numero1 * numero2);
+                break;
+            case 4:
+                 System.out.println(numero1 / numero2);
+                break;    
+            case 5:
+                 System.out.println("Are you sure you want to exit? Y/N");
+                  Scanner readmenu = new Scanner(System.in);
+        opcionsalir = readmenu.nextLine();
+                    if (opcionsalir.equalsIgnoreCase("Y")) 
+                    
